9aede78d336d083e07b6721cd3b78908a4e40e89
sergkharkin <sergkharkin@gmail.com>
2022-08-18 20:22:27 +0300

    вернул начальный index.htnl

--- wp-docker/index.php
+++ wp-docker/index.php
@@ -1,7 +1,7 @@
 <?php
-echo 'Поломался';
+/**echo 'Поломался';
 exit;
-
+*/
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
